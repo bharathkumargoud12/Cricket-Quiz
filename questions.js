6ce9e2d64e36640965ea0181929698f459f868ca
bharathkumargoud12 <87747439+bharathkumargoud12@users.noreply.github.com>
2021-07-21 11:37:46 +0530

    created questions for the quiz

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions=[{
+    "question":"Who is the present captain of Indian men's cricket team in ODI's ?",
+    "option1" :"M.S.Dhoni",
+    "option2" : "Rohit Sharma",
+    "option3" : "Virat Kohli" ,
+    "option4" : "Sachin Tendulkar",
+    "answer" : "3"
+},{
+    "question":"Where was the first edition of ICC Men's T20 Cricket World Cup held ?",
+    "option1" :"India",
+    "option2" : "England",
+    "option3" : "South Africa" ,
+    "option4" : "Australia",
+    "answer" : "3"
+},{
+    "question":"Which of the following statements regarding ICC Men's T20 Cricket World Cup is correct?",
+    "option1" :"The first ICC Men's T20 Cricket World Cup was held in 2008.",
+    "option2" : "Pakistan won the ICC Men's T20 Cricket World Cup in 2009.",
+    "option3" : " Sri Lanka won the 2012 ICC Men's T20 Cricket World Cup." ,
+    "option4" : "India has won the ICC Men's T20 Cricket World Cup 2 times so far",
+    "answer" : "2"
+},{
+    "question":"Which of the following country has not won a single title of ICC Men's T20 Cricket World Cup?",
+    "option1" :"Australia",
+    "option2" : "Pakistan",
+    "option3" : " Sri Lanka" ,
+    "option4" : "India",
+    "answer" : "1"
+}
+
+];
